d73d01b6419f4123a0f3816b2df7dee886c1fc86
Lionel Ovaert <lionel.ovaert@live.be>
2018-05-27 18:40:57 +0200

    Modified polling

--- background.js
+++ background.js
@@ -17,43 +17,79 @@ include(function () {
     $(document).ready(function () {
 
 
-        function pollFunc(fn, timeout, interval) {
-            var startTime = (new Date()).getTime();
-            interval = interval
-            timerOk = true;
+        // function pollFunc(fn, timeout, interval) {
+            // var startTime = (new Date()).getTime();
+            // interval = interval
+            // timerOk = true;
 
-            (function p() {
-                timerOk = ((new Date).getTime() - startTime) <= timeout;
-                if (!fn() && timerOk) { // ensures the function exucutes
-                    setTimeout(p, interval);
-                }
-            })();
-        }
+            // (function p() {
+                // timerOk = ((new Date).getTime() - startTime) <= timeout;
+                // if (!fn() && timerOk) { // ensures the function exucutes
+                    // setTimeout(p, interval);
+                // }
+            // })();
+        // }
 
-        pollFunc(checkOnline, 60000, 900000);
+        // pollFunc(checkOnline, 300000, 120000);
 
-        function checkOnline() {
-            console.log('Polling Twitch for OWL')
-            chrome.storage.sync.get('tabOpened', (data) => {
-                if (data['tabOpened'] === false) {
-                    $.ajax({
-                        type: "GET",
-                        url: 'https://api.twitch.tv/kraken/streams/overwatchleague?client_id=zub43tmi8ysviwt86oi6xjs6allpfh',
-                        data: '',
-                        success: function (data) {
-                            if (data['stream'] != null) {
-                                chrome.storage.sync.set({ tabOpened: true }, function () {
-                                    window.open('https://www.twitch.tv/overwatchleague', '_blank');
-                                });
-                            }
-                        },
-                        error: function (err) {
-                            console.log('Error when polling twitch api', err)
-                        }
-                    })
-                }
-            });
-        }
+        // function checkOnline() {
+            // console.log('Polling Twitch for OWL')
+			// var tab;
+            // chrome.storage.sync.get('tabOpened', (data) => {
+				// tab = data['tabOpened']
+				// console.log('tabOpened', tab);
+				// if (tab === false) {
+					// $.ajax({
+						// type: "GET",
+						// url: 'https://api.twitch.tv/kraken/streams/overwatchleague?client_id=zub43tmi8ysviwt86oi6xjs6allpfh',
+						// data: '',
+						// success: function (data) {
+							// console.log('data', data);
+							// if (data['stream'] !== null) {
+								// chrome.storage.sync.set({ tabOpened: true }, function () {
+									// console.log('Tab opened');
+									// window.open('https://www.twitch.tv/overwatchleague', '_blank');
+								// });
+							// }
+						// },
+						// error: function (err) {
+							// console.log('Error when polling twitch api', err)
+						// }
+					// })
+				// }
+			// });
+        // }
+		
+		(function checkOnline() {
+			setTimeout(function(){
+				console.log('Polling Twitch for OWL')
+				var tab;
+				chrome.storage.sync.get('tabOpened', (data) => {
+					tab = data['tabOpened']
+					console.log('tabOpened', tab);
+					if (tab === false) {
+						$.ajax({
+							type: "GET",
+							url: 'https://api.twitch.tv/kraken/streams/overwatchleague?client_id=zub43tmi8ysviwt86oi6xjs6allpfh',
+							data: '',
+							success: function (data) {
+								console.log('data', data);
+								if (data['stream'] !== null) {
+									chrome.storage.sync.set({ tabOpened: true }, function () {
+										console.log('Tab opened');
+										window.open('https://www.twitch.tv/overwatchleague', '_blank');
+									});
+								}
+							},
+							error: function (err) {
+								console.log('Error when polling twitch api', err)
+							},
+							complete: checkOnline
+						});
+					}
+				});
+			}, 2*60*1000);
+        })();
     })
 })
 
